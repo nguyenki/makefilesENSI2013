6ed062f9fec61970398ede75b6a1aa3d8a569ce4
KimThuatNGUYEN <kimthuatnguyen@kimthuatnguyen-VPCEB45FG.(none)>
2013-01-09 01:34:41 +0100

    0.8 parse makefile

--- sdmake.cpp
+++ sdmake.cpp
@@ -2,25 +2,69 @@
 #include <fstream>
 #include <cstdlib>
 #include <cstring>
-#include <mpi.h>
-#include <sdmake.h>
+#include <sstream>
+#include "sdmake.h"
 
-int main( int argc, char* argv[] )
-
+int main( int argc, char* argv[])
 {
+	cout <<"Hello world"<<"\n";
+}
 
 
-void parse(String &nameInputFile) {
+Rule *findRuleByName(const string &rule) {
+	Rule* r = rules[rule];
+	if (r==NULL) {
+		return rules[rule] = new Rule(rule);
+	} else {
+		return r;
+	}
+}
+
+void addDependency(Rule *rule, const string &dependencyName) {
+	Rule *newDependency = rules[rule];
+	rule->dependences.push_back(newDependency);
+	newDependency->dependants.push_back(rule);
+}
+
+void parse(string &nameInputFile) {
+	char* line[LINE_LENGTH];
+	string ruleName="";
+	Rule *rule;
+	stringstream line_temp;	
+
 	ifstream inFile;
-	inFile.open(nameInputFile);
+	inFile.open(nameInputFile.c_str(), ifstream::in);
 	if (inFile.bad()) {
-		cerr <<"Bad bit is set in file"<< endl << endl;
+		cerr <<"Bad bit is set in Makefile"<< endl << endl;
 		exit(BAD_FILE);
 	}
 	while (!inFile.eof()) {
-		
+		inFile.getline(line, LINE_LENGTH);
+		line_temp << line;
+		if (strlen(line)==0) // Laisse tomber la ligne vide
+			continue;
+		if (line[0]=='#') // Saute la ligne de commentaire
+			continue;
+		if (line[0]!='\t') { // Si la ligne ne commence pas avec un tabulation. Alors, c'est un regle
+			line_temp >> ruleName;
+			// Si ce n'est pas une ligne qui a un cible suivant par un : ==> ignore le.
+			if (ruleName.length() <2 && ruleName[ruleName.size()]!=':')
+				continue;
+			ruleName = ruleName.substr(0, ruleName.size()-1);
+			rule = findRuleByName(ruleName);
+			string dependency;
+			while (line_temp >> dependency) {
+				addDependency(rule, dependency);
+			}
+		} else {
+			rule->command.push_back(line);
+		}
 	}
-	
+	inFile.close();
+}
+
+int getParameterCommandLine(int argc, char* argv[]) {
+
 }
 
 
--- sdmake.h
+++ sdmake.h
@@ -5,15 +5,47 @@
 #include <cstdlib>
 
 #include <cstring>
-
-
+#include <vector>
+#include <iostream>
+#include <list>
+#include <map>
 
 #include <mpi.h>
 
 #define BAD_FILE -1
+#define LINE_LENGTH 32687
+
 using namespace std;
 
-void parse(String &nameInputFile);
+const string CORRECT_INPUT = "sdmake makefile_name";
 
-void parseCommand(int argc, char* argv[]);
+/* Struct Rule pour presenter une ligne (une regle) du MakeFile
+ * Une regle depend aux autre regles et a aussi des dependances
+*/
+struct Rule {
+	int idRule;
+	string name;
+	vector<int> command; // La ligne de la commande suivant une regle
+	bool isExecuted;
+	bool isFinished;
+	list<Rule*> dependences;
+	list<Rule*> dependants;
+	Rule (string ruleName): isExecuted(false), isFinished(false), name(ruleName) { }
+};
 
+map<string, Rule*> rules;
+vector<Rule*> tasks;
+
+// Chercher une regle using son nom. Retourner un nouveau regle si'il n'existe pas
+Rule *findRuleByName(const string &rule);
+
+// Ajouter un regle dependant d'un autre regle
+void addDependency(Rule* rule, const string &dependencyName);
+
+// Parser un fichier Makefile simple
+void parse(string &nameInputFile);
+
+// Lire la commande entree par l'utilisateur
+int getParameterCommandLine(int argc, char* argv[]);
+
+#endif
